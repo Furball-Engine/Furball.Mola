4a9da6f518ba655fcc16f8413aff1829755499a0
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-09-11 12:33:56 -0700

    Working triangle rasterization...

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const Types = @import("types.zig");
 const RenderBitmap = Types.RenderBitmap;
 
 const Vector2 = @import("types.zig").Vector2;
+const Vector2i = @import("types.zig").Vector2i;
 const Color = @import("pixel_types.zig").rgba128;
 const Vertex = @import("types.zig").Vertex;
 
@@ -32,18 +33,18 @@ export fn create_render_bitmap(width: c_uint, height: c_uint, pixel_type: pixel_
 }
 
 export fn clear_render_bitmap(bitmap: *RenderBitmap) callconv(.C) void {
-    var i: usize = 0;
+    var j: usize = 0;
     var pixel_count: c_uint = bitmap.width * bitmap.height;
 
     switch (bitmap.pixel_type) {
         pixel_types.pixel_type.rgba32 => {
-            while (i < pixel_count) : (i += 1) {
-                bitmap.rgba32ptr[i] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
+            while (j < pixel_count) : (j += 1) {
+                bitmap.rgba32ptr[j] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
             }
         },
         pixel_types.pixel_type.argb32 => {
-            while (i < pixel_count) : (i += 1) {
-                bitmap.argb32ptr[i] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
+            while (j < pixel_count) : (j += 1) {
+                bitmap.argb32ptr[j] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
             }
         },
     }
@@ -64,51 +65,68 @@ export fn draw_onto_bitmap(bitmap: *RenderBitmap, vertices: [*]Vertex, indices: 
     if (index_count % 3 != 0)
         @panic("Index count is not a multiple of 3");
 
-    var i: usize = 0;
+    var j: usize = 0;
     //Iterate through all indices
-    while (i < index_count) : (i += 3) {
+    while (j < index_count) : (j += 3) {
         //Rasterize each triangle
-        rasterize_triangle(bitmap, vertices[indices[i]], vertices[indices[i + 1]], vertices[indices[i + 2]]);
+        rasterize_triangle(bitmap, vertices[indices[j]], vertices[indices[j + 1]], vertices[indices[j + 2]]);
     }
+}
+
+fn f(int: i32) f32 {
+    return @intToFloat(f32, int);
+}
+
+fn i(float: f32) i32 {
+    return @floatToInt(i32, float);
 }
 
 export fn rasterize_triangle(bitmap: *RenderBitmap, vtx1: Vertex, vtx2: Vertex, vtx3: Vertex) callconv(.C) void {
-    var ax0: i32 = @floatToInt(i32, vtx1.position.x);
-    var ay0: i32 = @floatToInt(i32, vtx1.position.y);
-    var ax1: i32 = @floatToInt(i32, vtx2.position.x);
-    var ay1: i32 = @floatToInt(i32, vtx2.position.y);
-    var ax2: i32 = @floatToInt(i32, vtx3.position.x);
-    var ay2: i32 = @floatToInt(i32, vtx3.position.y); 
-    
-    if (ay0>ay1) {
-        swap_ints(&ax0, &ax1);
-        swap_ints(&ay0, &ay1);
-    }
-    if (ay0>ay2) {
-        swap_ints(&ax0, &ax2);
-        swap_ints(&ay0, &ay2);
-    }
-    if (ay1>ay2) {
-        swap_ints(&ax1, &ax2);
-        swap_ints(&ay1, &ay2);
-    }
+    var t0: Vector2i = .{.x = @floatToInt(i32, vtx1.position.x), .y = @floatToInt(i32, vtx1.position.y)};
+    var t1: Vector2i = .{.x = @floatToInt(i32, vtx2.position.x), .y = @floatToInt(i32, vtx2.position.y)};
+    var t2: Vector2i = .{.x = @floatToInt(i32, vtx3.position.x), .y = @floatToInt(i32, vtx3.position.y)};
 
-    rasterize_line(bitmap, .{.x = @intToFloat(f32, ax0), .y = @intToFloat(f32, ay0)}, .{.x = @intToFloat(f32, ax1), .y = @intToFloat(f32, ay1)}, .{.r = 0, .g = 255, .b = 0, .a = 255});
-    rasterize_line(bitmap, .{.x = @intToFloat(f32, ax1), .y = @intToFloat(f32, ay1)}, .{.x = @intToFloat(f32, ax2), .y = @intToFloat(f32, ay2)}, .{.r = 0, .g = 255, .b = 0, .a = 255});
-    rasterize_line(bitmap, .{.x = @intToFloat(f32, ax2), .y = @intToFloat(f32, ay2)}, .{.x = @intToFloat(f32, ax0), .y = @intToFloat(f32, ay0)}, .{.r = 255, .g = 0, .b = 0, .a = 255});
-}
+    if (t0.y>t1.y) std.mem.swap(Vector2i, &t0, &t1);
+    if (t0.y>t2.y) std.mem.swap(Vector2i, &t0, &t2);
+    if (t1.y>t2.y) std.mem.swap(Vector2i, &t1, &t2);
 
-fn swap_ints(x: *i32, y: *i32) void {
-    //Set X to the sum of the 2
-    x.* = x.* + y.*;
-    //Get the original X value out and place it in Y
-    y.* = x.* - y.*;
-    //Get the original Y value out using the original X and place it in X
-    x.* = x.* - y.*;
+    var total_height: i32 = t2.y-t0.y; 
+    var y: i32 = t0.y;
+    while(y <= t1.y) : (y += 1) {
+        var segment_height: i32 = t1.y-t0.y+1; 
+        var alpha: f32 = f(y-t0.y)/f(total_height); 
+        var beta: f32  = f(y-t0.y)/f(segment_height); // be careful with divisions by zero 
+        var a: Vector2i = .{.x = i(f(t0.x) + f(t2.x-t0.x)*alpha), .y = i(f(t0.y) + f(t2.y-t0.y)*alpha)}; 
+        var b: Vector2i = .{.x = i(f(t0.x) + f(t1.x-t0.x)*beta), .y = i(f(t0.y) + f(t1.y-t0.y)*beta)}; 
+        if (a.x>b.x) std.mem.swap(Vector2i, &a, &b); 
+        var j: i32 = a.x;
+        while(j <= b.x) : (j += 1) {
+            set_bitmap_pixel(bitmap, j, y, .{.r = 255, .g = 255, .b = 255, .a = 255});
+            // image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+        } 
+    }   
+    y = t1.y;
+    while(y <= t2.y) : (y += 1) {
+        var segment_height: i32 =  t2.y-t1.y+1; 
+        var alpha: f32 = f(y-t0.y)/f(total_height); 
+        var beta: f32  = f(y-t1.y)/f(segment_height); // be careful with divisions by zero 
+        var a: Vector2i = .{.x = i(f(t0.x) + f(t2.x-t0.x)*alpha), .y = i(f(t0.y) + f(t2.y-t0.y)*alpha)}; 
+        var b: Vector2i = .{.x = i(f(t1.x) + f(t2.x-t1.x)*beta), .y = i(f(t1.y) + f(t2.y-t1.y)*beta)}; 
+        if (a.x>b.x) std.mem.swap(Vector2i, &a, &b); 
+        var j: i32 = a.x;
+        while(j <= b.x) : (j += 1) {
+            set_bitmap_pixel(bitmap, j, y, .{.r = 255, .g = 255, .b = 255, .a = 255});
+            // image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+        } 
+    } 
+
+    // rasterize_line(bitmap, .{.x = @intToFloat(f32, ax0), .y = @intToFloat(f32, ay0)}, .{.x = @intToFloat(f32, ax1), .y = @intToFloat(f32, ay1)}, .{.r = 0, .g = 255, .b = 0, .a = 255});
+    // rasterize_line(bitmap, .{.x = @intToFloat(f32, ax1), .y = @intToFloat(f32, ay1)}, .{.x = @intToFloat(f32, ax2), .y = @intToFloat(f32, ay2)}, .{.r = 0, .g = 255, .b = 0, .a = 255});
+    // rasterize_line(bitmap, .{.x = @intToFloat(f32, ax2), .y = @intToFloat(f32, ay2)}, .{.x = @intToFloat(f32, ax0), .y = @intToFloat(f32, ay0)}, .{.r = 255, .g = 0, .b = 0, .a = 255});
 }
 
 export fn set_bitmap_pixel(bitmap: *RenderBitmap, x: i32, y: i32, col: pixel_types.rgba32) callconv(.C) void {
-    if(x >= bitmap.width or y >= bitmap.height)
+    if (x >= bitmap.width or y >= bitmap.height)
         return;
 
     var pos = (@intCast(usize, y) * @intCast(usize, bitmap.width)) + @intCast(usize, x);
@@ -126,46 +144,46 @@ export fn set_bitmap_pixel(bitmap: *RenderBitmap, x: i32, y: i32, col: pixel_typ
     }
 }
 
-export fn rasterize_line(bitmap: *RenderBitmap, p0: Vector2, p1: Vector2, col: pixel_types.rgba32) callconv(.C) void {
-    var ax0: i32 = @floatToInt(i32, p0.x);
-    var ay0: i32 = @floatToInt(i32, p0.y);
-    var ax1: i32 = @floatToInt(i32, p1.x);
-    var ay1: i32 = @floatToInt(i32, p1.y);
+// export fn rasterize_line(bitmap: *RenderBitmap, p0: Vector2, p1: Vector2, col: pixel_types.rgba32) callconv(.C) void {
+//     var ax0: i32 = @floatToInt(i32, p0.x);
+//     var ay0: i32 = @floatToInt(i32, p0.y);
+//     var ax1: i32 = @floatToInt(i32, p1.x);
+//     var ay1: i32 = @floatToInt(i32, p1.y);
 
-    std.debug.print("Drawing line with points {d}x{d},{d}x{d}\n", .{ax0, ay0, ax1, ay1});
+//     std.debug.print("Drawing line with points {d}x{d},{d}x{d}\n", .{ ax0, ay0, ax1, ay1 });
 
-    var steep: bool = false;
-    //Check whether the the difference in X is less than the difference in Y (aka is slope greater than 0.5)
-    if ((std.math.absInt(ax0 - ax1) catch @panic("Unable to ABS x0 and x1")) < (std.math.absInt(ay0 - ay1) catch @panic("Unable to ABS y0 and y1"))) {
-        swap_ints(&ax0, &ay0);
-        swap_ints(&ax1, &ay1);
-        steep = true;
-    }
-    //If the first point is to the right of the second point, swap the points
-    if (ax0 > ax1) {
-        swap_ints(&ax0, &ax1);
-        swap_ints(&ay0, &ay1);
-    }
-    //Get the difference in X
-    var dx: i32 = ax1 - ax0;
-    //Get the difference in Y
-    var dy: i32 = ay1 - ay0;
-    //The error in difference
-    var derror2: i32 = (std.math.absInt(dy) catch @panic("Unable to ABS dy")) * 2;
-    //The distance to the best straight line from our current position
-    var error2: i32 = 0;
-    var y: i32 = ay0;
-    var x: i32 = ax0;
-    while (x <= ax1) : (x += 1) {
-        if (steep) {
-            set_bitmap_pixel(bitmap, y, x, col);
-        } else {
-            set_bitmap_pixel(bitmap, x, y, col);
-        }
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (if (ay1 > ay0) 1 else -1);
-            error2 -= dx * 2;
-        }
-    }
-}
+//     var steep: bool = false;
+//     //Check whether the the difference in X is less than the difference in Y (aka is slope greater than 0.5)
+//     if ((std.math.absInt(ax0 - ax1) catch @panic("Unable to ABS x0 and x1")) < (std.math.absInt(ay0 - ay1) catch @panic("Unable to ABS y0 and y1"))) {
+//         swap_ints(&ax0, &ay0);
+//         swap_ints(&ax1, &ay1);
+//         steep = true;
+//     }
+//     //If the first point is to the right of the second point, swap the points
+//     if (ax0 > ax1) {
+//         swap_ints(&ax0, &ax1);
+//         swap_ints(&ay0, &ay1);
+//     }
+//     //Get the difference in X
+//     var dx: i32 = ax1 - ax0;
+//     //Get the difference in Y
+//     var dy: i32 = ay1 - ay0;
+//     //The error in difference
+//     var derror2: i32 = (std.math.absInt(dy) catch @panic("Unable to ABS dy")) * 2;
+//     //The distance to the best straight line from our current position
+//     var error2: i32 = 0;
+//     var y: i32 = ay0;
+//     var x: i32 = ax0;
+//     while (x <= ax1) : (x += 1) {
+//         if (steep) {
+//             set_bitmap_pixel(bitmap, y, x, col);
+//         } else {
+//             set_bitmap_pixel(bitmap, x, y, col);
+//         }
+//         error2 += derror2;
+//         if (error2 > dx) {
+//             y += (if (ay1 > ay0) 1 else -1);
+//             error2 -= dx * 2;
+//         }
+//     }
+// }
